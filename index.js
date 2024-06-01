e8cfa59651cb6b572d08e6d0737038868a668c55
NULLVOID76 <nullvoid76@gmail.com>
2024-06-01 08:08:09 +0530

    axios removed

--- index.js
+++ index.js
@@ -39,29 +39,28 @@ app.post("/webhook", (req, res) => {
       body_param.entry[0].changes &&
       body_param.entry[0].changes[0].value.messages &&
       body_param.entry[0].changes[0].value.messages[0]
-    ) {
-      let phon_no_id = body_param.entry[0].changes[0].value.metadata.phone_number_id;
+    ) 
+    {
       let from = body_param.entry[0].changes[0].value.messages[0].from;
       let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
 
-      console.log("phone number " + phon_no_id);
       console.log("from " + from);
       console.log("boady param " + msg_body);
 
-      axios({
-        method: "POST",
-        url: ` https://graph.facebook.com/v13.0/${phon_no_id}/messages?access_token=${process.env.TOKEN} `,
-        data: {
-          messaging_product: "whatsapp",
-          to: from,
-          text: {
-            body: "Hi.. we're tech titans, your message is " + msg_body,
-          },
-        },
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
+    //   axios({
+    //     method: "POST",
+    //     url: ` https://graph.facebook.com/v13.0/${process.env.PHONE_NUMBER_ID}/messages?access_token=${process.env.TOKEN} `,
+    //     data: {
+    //       messaging_product: "whatsapp",
+    //       to: from,
+    //       text: {
+    //         body: "Hi.. we're tech titans, your message is " + msg_body,
+    //       },
+    //     },
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //     },
+    //   });
       res.sendStatus(200);
     } 
     else 
