353d2dce5da6358f2b0dffda6ee27f8e427cf96c
NULLVOID76 <nullvoid76@gmail.com>
2024-06-01 22:56:22 +0530

    custom msg

--- keyword.js
+++ keyword.js
@@ -1,8 +1,8 @@
 function detect ( s ){
-
-    if(s.includes("College"))
+    s=s.toLowerCase();
+    if(s.includes("college"))
         return "MMMUT Gotakhpur"
-    if(s.includes("Date"))
+    if(s.includes("date"))
         return "2/06/2024";
 }
 
