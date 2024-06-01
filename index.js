e44e57da1865273d2289e6070477b018c3d50d73
NULLVOID76 <nullvoid76@gmail.com>
2024-06-01 08:04:19 +0530

    update 2

--- index.js
+++ index.js
@@ -30,10 +30,10 @@ app.post("/webhook", (req, res) => {
 
   let body_param = req.body;
 
-  console.log(JSON.stringify(body_param, null, 2));
+//   console.log(JSON.stringify(body_param, null, 2));
 
   if (body_param.object) {
-    console.log("inside body param");
+    // console.log("inside body param");
     if (
       body_param.entry &&
       body_param.entry[0].changes &&
