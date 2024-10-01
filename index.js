43f69d06259e1eb835752ffb18fa150cd832a49c
NULLVOID76 <nullvoid76@gmail.com>
2024-10-01 14:03:47 +0530

    new test change

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import axios from "axios";
 import bodyParser from "body-parser";
 
 const app = express().use(bodyParser.json());
-import detect from "./keyword.js";
 
 // let MY_TOKEN="Tech_titans";
 
@@ -12,10 +11,6 @@ import detect from "./keyword.js";
 app.listen(process.env.PORT, () => {
   console.log(`wehook server is ready ${process.env.PORT}`);
 });
-app.get("/",(req,res)=>{
-  console.log("Manvendra");
-  
-})
 app.get("/webhook", (req, res) => {
   let mode = req.query["hub.mode"];
   let challange = req.query["hub.challenge"];
@@ -31,20 +26,20 @@ app.get("/webhook", (req, res) => {
 });
 
 app.post("/webhook", (req, res) => {
-
+  console.log("msgg\n");
 
   let body = req.body;
-  let Link=  ` https://graph.facebook.com/v13.0/${process.env.PHONE_NUMBER_ID}/messages?access_token=${process.env.TOKEN} `;
-
-
+  let Link = ` https://graph.facebook.com/v13.0/${process.env.PHONE_NUMBER_ID}/messages?access_token=${process.env.TOKEN} `;
 
   if (body.object) {
     if (
-      body.entry && body.entry[0].changes && body.entry[0].changes[0].value.messages && body.entry[0].changes[0].value.messages[0]
+      body.entry &&
+      body.entry[0].changes &&
+      body.entry[0].changes[0].value.messages &&
+      body.entry[0].changes[0].value.messages[0]
     ) {
-       
-  console.log(JSON.stringify(body, null, 1));
-      let prev_msg_id= body.entry[0].changes[0].value.messages[0].id;
+      console.log(JSON.stringify(body, null, 1));
+      let prev_msg_id = body.entry[0].changes[0].value.messages[0].id;
       axios({
         method: "POST",
         url: Link,
@@ -58,55 +53,52 @@ app.post("/webhook", (req, res) => {
       let from = body.entry[0].changes[0].value.messages[0].from;
       let name = body.entry[0].changes[0].value.contacts[0].profile.name;
 
-
-
       // let msg_body = body.entry[0].changes[0].value.messages[0].text.body;
-      // console.log(`from  ${from} \nname ${name} \nbody msg  ${msg_body}`);
-      // let msg;
-      // msg = detect(msg_body);
+      console.log(`from  ${from} \nname ${name} \nbody msg  ${msg_body}`);
 
-      let greeting = {
-        messaging_product: "whatsapp",
-        to: from,
-        context: {
-          message_id: prev_msg_id,
-        },
-        type: "template",
-        template: {
-          name: "greeting",
-          language: {
-            code: "en_GB",
-            policy: "deterministic",
-          },
-          components: [
-            {
-              type: "body",
-              parameters: [
-                {
-                  type: "text",
-                  text: `${name}`,
-                },
-              ],
-            },
-          ],
-        },
-        headers: {
-          "Content-Type": "application/json",
-        },
-      };
-      
+      // let greeting = {
+      //   messaging_product: "whatsapp",
+      //   to: from,
+      //   context: {
+      //     message_id: prev_msg_id,
+      //   },
+      //   type: "template",
+      //   template: {
+      //     name: "greeting",
+      //     language: {
+      //       code: "en_GB",
+      //       policy: "deterministic",
+      //     },
+      //     components: [
+      //       {
+      //         type: "body",
+      //         parameters: [
+      //           {
+      //             type: "text",
+      //             text: `${name}`,
+      //           },
+      //         ],
+      //       },
+      //     ],
+      //   },
+      //   headers: {
+      //     "Content-Type": "application/json",
+      //   },
+      // };
 
       axios({
         method: "POST",
         url: Link,
-        data: greeting,
-        /*
-         text: {
+        data: {
+          messaging_product: "whatsapp",
+          to: from,
+          type: "text",
+          text: {
             body: msg
               ? msg
               : `Hello ${name} \nWe're tech titans, \nYour Message is > ${msg_body} `,
           },
-        */
+        },
       });
       res.sendStatus(200);
     } else {
@@ -114,5 +106,3 @@ app.post("/webhook", (req, res) => {
     }
   }
 });
-
-
